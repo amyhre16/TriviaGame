0bd02da50c8aa7c6fc4d14d252384d06bce042b3
Anthony Myhre <anthony.myhre@utexas.edu>
2016-11-19 21:26:31 -0600

    added a row after each question to display the result from each question. I got it to show, but it's displaying 10 of the result instead of just one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
-	var correct = 0;
+   //  create vars
+    var correct = 0;
 	var wrong = 0;
 	var unanswered = 0;
+    var result;
 	var timeLeft = 10;
 	var time;
-    function timeDown() {
-        $("#timeRemaining").html(timeLeft);
-        time = setTimeout(timeDown, 1000);
-	    if (timeLeft == 0) {
-	    	console.log("done");
-	    	clearTimeout(time);
-	    	$('.question').hide();
-	    	reset();
-	    }
-        timeLeft--;
-    }
-    
-    // startTime();
 
+
+    // function timeDown() {
+    //     $("#timeRemaining").html(timeLeft);
+    //     time = setTimeout(timeDown, 1000);
+	   //  if (timeLeft == 0) {
+	   //  	console.log("done");
+	   //  	clearTimeout(time);
+	   //  	$('.question').hide();
+	   //  	reset();
+	   //  }
+    //     timeLeft--;
+    // }
+    
+    // starts the timer
     function startTime() {
+    	timeLeft = 10;
     	time = setInterval(count, 1000);
     }
 
+    // increments and displays the remaining time
     function count() {
     	$('#timeRemaining').html(timeLeft);
-    	if (timeLeft < 0) {
+    	if (timeLeft == 0) {
+            result = $('.current').children('.correct');
+            displayCorrectAnswer();
     		reset();
     	}
     	else {
@@ -32,26 +39,57 @@ $(document).ready(function() {
 		}
     }
 
+    // clears the time interval
     function reset() {
     	clearInterval(time);
-    	timeLeft = 10;
-    	$('#timeRemaining').html(timeLeft);
+    	// timeLeft = 10;
+    	// $('#timeRemaining').html(timeLeft);
     	// startTime();
     }
 
-    $('.answer').on('click', function() {
-    	// console.log($(this).parent().next('.question').attr('id'));
-    	$(this).parent().removeClass('current');
-    	$(this).parent().hide();
-    	$(this).parent().next('.question').show();
-    	$(this).parent().next('.question').addClass('current');
-    	reset();
-    });
-
+    // event listener for the start button
     $('#start').on('click', function() {
     	$(this).hide();
     	$(this).prop('disabled', true);
     	$(this).next().show();
     	startTime();
     });
+
+    // function to display the question result
+    function displayCorrectAnswer() {
+        $('.result').html(result);
+
+    }
+
+    // event listener for when the user clicks on an answer
+    $('.answer').on('click', function() {
+        // console.log($(this).parent().next('.question').attr('id'));
+        // if the answer chosen is correct, assign a header to the result variable and display it
+        if ($(this).hasClass('correct')) {
+            result = "<h2>Correct!</h2>";
+            displayCorrectAnswer();
+            console.log("Correct");
+        }
+        // if the incorrect answer is chosen, assign a header to the result var and display it
+        else {
+            result = "<h2>Incorrect!</h2><h4>The answer we were looking for was " + $(this).parent().next('.correct').text() +"</h4>";
+            displayCorrectAnswer();
+            console.log("Wrong!");
+        }
+        // remove the current class from this question and hide it
+        $(this).parent().removeClass('current');
+        $(this).parent().hide();
+        // $(this).parent().next().next('.question').show();
+
+        // add current class to the next question
+        $(this).parent().next().next('.question').addClass('current');
+        // show the correct answer div
+        $(this).parent().next().show();
+
+        // clear the timer
+        clearInterval(time);
+        // startTime();
+        // reset();
+    });
+
 });
